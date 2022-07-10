858957caf8701c4c8614e590bf6c30101c49b740
Santosl2c <mfilype2019@gmail.com>
2022-07-10 16:52:27 -0300

    fix: input width wrong

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -2,17 +2,8 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/function-component-definition */
 /* eslint-disable react/jsx-props-no-spreading */
-import React, {
-  forwardRef,
-  ForwardRefRenderFunction,
-  useCallback,
-  useMemo,
-  useState,
-} from "react";
+import { forwardRef, ForwardRefRenderFunction, useMemo } from "react";
 
-import { Upload } from "phosphor-react";
-
-import { ALLOWED_UPLOAD_TYPES } from "@/shared/constants";
 import { classNames } from "@/shared/utils/classNames";
 
 import { CopyButton } from "../CopyButton";
@@ -24,7 +15,6 @@ const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
   ref
 ) => {
   const hasError = !!error;
-  const [currentFile, setCurrentFile] = useState<File>();
 
   const buttonClasses = useMemo(() => {
     return `${classNames({
@@ -32,47 +22,17 @@ const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
       "input-bordered": true,
       "w-full": true,
       "text-red-error": hasError,
-      customInputFile: props.type === "file",
     })} ${props.className}`;
   }, [hasError, props.className]);
 
-  const onUpdateFile = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    const { target } = e;
-
-    const file = target.files?.item(0);
-
-    if (!file) {
-      return;
-    }
-    if (ALLOWED_UPLOAD_TYPES.includes(file.type)) {
-      setCurrentFile(file);
-    }
-  }, []);
-
   return (
     <>
-      {label &&
-        (props.type !== "file" ? (
-          <Label title={label} htmlFor={id ?? ""} hasError={hasError} />
-        ) : (
-          <label
-            htmlFor={id}
-            className="btn flex items-center gap-2 bg-purple-800 text-white hover:bg-purple-900 hover:border-purple-900 border-purple-800"
-          >
-            <Upload size={20} /> {currentFile?.name ? currentFile.name : label}
-          </label>
-        ))}
+      {label && <Label title={label} htmlFor={id ?? ""} hasError={hasError} />}
 
-      <div className="relative">
-        <input
-          {...props}
-          ref={ref}
-          id={id}
-          className={buttonClasses}
-          onChange={props.type === "file" ? onUpdateFile : () => {}}
-        />
+      <div className="relative w-full">
+        <input {...props} ref={ref} id={id} className={buttonClasses} />
         {hasError && <p className="self-start mt-3 text-red-error">{error}</p>}
-        {enableCopyButton && id && <CopyButton copyId={id} />}
+        {enableCopyButton && id && <CopyButton copyId={id} isInput />}
       </div>
     </>
   );
